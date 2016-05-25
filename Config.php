67d0bc49a24262f337b528da0427aeb62dbc1ebd
Kelvin Pereira Alves <kelvinpalves@gmail.com>
2016-05-24 21:09:43 -0300

    Arquivo com as váriaveis estáticas do programa.

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+
+class Config {
+
+	const DB_HOST 	= '127.0.0.1';
+	const DB_NAME 	= 'd3sconn3ct_shorter';
+	const DB_USER 	= 'root';
+	const DB_PASSWD = '';
+	const BASE_URL 	= 'http://www.google.com.br';
+
+}
